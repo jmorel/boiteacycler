4b2c2fc1a5c448175de81abdbbb85217e4ac4e68
Jeremy Morel <jeremy.morel@opendatasoft.com>
2016-11-13 18:23:05 +0100

    moving to a grav theme

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+
+gulp.task('default', ['watch']);
+
+gulp.task('less', function () {
+    return gulp.src('./less/*.less')
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(concat('boiteacycler.css'))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css-compiled'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch('./less/*.less', ['less'])
+});
