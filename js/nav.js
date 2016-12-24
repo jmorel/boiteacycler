d904f9eca1c5bb5271d4095742bf73e70cf9730f
Jeremy Morel <jeremy.morel@opendatasoft.com>
2016-12-24 15:47:56 +0100

    fix navigation

--- js/nav.js
+++ js/nav.js
@@ -9,24 +9,23 @@
     }
 
     var i;
-    var sectionElements = [];
+    var sections = [];
     for (i = 0; i < navItemElements.length; i++) {
         var href = navItemElements[i].href;
         var sectionId = href.substring(href.indexOf('#') + 1, href.length);
         var sectionElement = document.getElementById(sectionId);
-        sectionElements.push({
+        sections.push({
             element: sectionElement,
             navElement: navItemElements[i],
-            id: sectionId,
-            offsetTop: sectionElement.offsetTop
+            id: sectionId
         });
     }
 
-    var setCurrentNavItem = function (sectionElement) {
-        for (i=0; i < sectionElements.length; i++) {
-            sectionElements[i].navElement.classList.remove('active');
+    var setCurrentNavItem = function (section) {
+        for (i=0; i < sections.length; i++) {
+            sections[i].navElement.classList.remove('active');
         }
-        sectionElement.navElement.classList.add('active');
+        section.navElement.classList.add('active');
     };
 
     var updateNavBar = function () {
@@ -37,10 +36,10 @@
             navElement.classList.add('nav--wide');
         }
 
-        for (i = sectionElements.length - 1; i >= 0; i--) {
-            var sectionElement = sectionElements[i];
-            if (sectionElement.offsetTop + 40 < window.innerHeight + window.scrollY) {
-                setCurrentNavItem(sectionElement);
+        for (i = sections.length - 1; i >= 0; i--) {
+            var section = sections[i];
+            if (section.element.offsetTop + 40 < window.innerHeight + window.scrollY) {
+                setCurrentNavItem(section);
                 break;
             }
         }
